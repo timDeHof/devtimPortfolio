08e2242c74ea83b72497d202814562651c405368
timDeHof <this.is.tim.dehof@gmail.com>
2022-07-10 19:05:57 -0400

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-
+import React from 'react';
+import Button from '@mui/material/Button';
 function App() {
   return (
     <main className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="contained">Hello World</Button>
     </main>
   );
 }
