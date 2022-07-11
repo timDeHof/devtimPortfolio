6e47000c496a67aba1b1f089ca806ef107b647ed
timDeHof <this.is.tim.dehof@gmail.com>
2022-07-11 16:43:45 -0400

    setting up tailwind

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="fixed top-0">
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#01192E',
+        secondary: '#FFFFFF',
+        blue2: '#0E3460',
+        blue3: '#4DB5FF',
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
